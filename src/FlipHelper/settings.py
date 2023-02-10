ff5217b446cf7c152db179e50e95d789d6430c26
Neruay <Neruayyy@gmail.com>
2023-02-10 07:43:48 +0200

    Add missing files

---
+++ src/FlipHelper/settings.py
@@ -0,0 +1,7 @@
+BULK_SIZE = 1
+LEAGUE = "Sanctum"
+
+# Account info
+POE_SESSION_ID = "8dba69cca28944fa63cb02e29b454124"
+USER_AGENT = "PoEFlipHelper/neruayyy@gmail.com"
+API_TOKEN = "8e1f6a41f50c145705f50b954467dd921946b468"
